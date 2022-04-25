9166fb63ff65293147f9b61a461d22d33a4b0fec
kivan <kirill.ivanin00@yandex.ru>
2022-04-25 09:36:46 +0300

    Add project files.

---
+++ norming_planing_wpf_core/AcszmkdbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace norming_planing_wpf_core
+{
+    public class AcszmkdbContext: DbContext
+    {
+        public DbSet<Draft> Drafts { get; set; }
+     
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=acszmkdb;Username=postgres;Password=postgres");
+        }
+    }
+}
---
+++ norming_planing_wpf_core/Models.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace norming_planing_wpf_core
+{
+    public class Draft
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
